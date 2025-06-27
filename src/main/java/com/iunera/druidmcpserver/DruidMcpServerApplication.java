195f07fcbd23f83ddd32d6930da3817b60a05a0c
Christian Schmitt <christian.schmitt@iunera.com>
2025-06-27 17:38:05 +0200

    first commit

---
+++ src/main/java/com/iunera/druidmcpserver/DruidMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.iunera.druidmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DruidMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iunera/druidmcpserver/DruidMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iunera.druidmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DruidMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
